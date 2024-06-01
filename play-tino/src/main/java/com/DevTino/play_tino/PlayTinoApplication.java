eac8d1b49ed51d592f7f68381b8faf163c9becb8
Lee <lsmin476@naver.com>
2024-06-01 20:35:33 +0900

    |ADD| 프로젝트 생성 및 프로젝트 설정

---
+++ play-tino/src/main/java/com/DevTino/play_tino/PlayTinoApplication.java
@@ -0,0 +1,13 @@
+package com.DevTino.play_tino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayTinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayTinoApplication.class, args);
+	}
+
+}
---
+++ play-tino/src/test/java/com/DevTino/play_tino/PlayTinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevTino.play_tino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayTinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
